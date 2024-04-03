ef1526991ed25692ac034cfeebca1ed239e4947b
Krishna <krishna.rowter@gmail.com>
2024-04-03 08:40:34 +0700

    finish undo, todo redo

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,105 +1,138 @@
-import { LitElement, css, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { controllerCensorOption } from './symbols';
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { controllerCensorOption } from "./symbols";
 
-
-function createCanvasActions(canvas: HTMLCanvasElement, canvasCtx: CanvasRenderingContext2D) {
+function createCanvasActions(
+  canvas: HTMLCanvasElement,
+  canvasCtx: CanvasRenderingContext2D
+) {
   return {
-    'blur': (startX: number, startY: number, endX: number, endY: number) => {
-      const latestCanvasImage = new Image()
+    blur: (startX: number, startY: number, endX: number, endY: number) => {
+      const latestCanvasImage = new Image();
       latestCanvasImage.onload = () => {
-        canvasCtx.filter = 'blur(5px)'
+        canvasCtx.filter = "blur(5px)";
 
-        const selectedArea = [startX, startY, endX - startX, endY - startY] as const
+        const selectedArea = [
+          startX,
+          startY,
+          endX - startX,
+          endY - startY,
+        ] as const;
 
-        canvasCtx.drawImage(latestCanvasImage, ...selectedArea, ...selectedArea)
+        canvasCtx.drawImage(
+          latestCanvasImage,
+          ...selectedArea,
+          ...selectedArea
+        );
 
-        canvasCtx.filter = 'none'
-      }
-      latestCanvasImage.src = canvas.toDataURL()
+        canvasCtx.filter = "none";
+      };
+      latestCanvasImage.src = canvas.toDataURL();
     },
-    'black-out': (startX: number, startY: number, endX: number, endY: number) => {
-      canvasCtx.rect(startX, startY, endX - startX, endY - startY);
-      canvasCtx.fill()
+    "black-out": (
+      startX: number,
+      startY: number,
+      endX: number,
+      endY: number
+    ) => {
+      canvasCtx.fillRect(startX, startY, endX - startX, endY - startY);
+      canvasCtx.closePath();
+    },
+  };
+}
+
+type CanvasDrawEvent =
+  | {
+      type: "blur";
+      payload: [startX: number, startY: number, endX: number, endY: number];
     }
-  }
-}
+  | {
+      type: "black-out";
+      payload: [startX: number, startY: number, endX: number, endY: number];
+    };
 
-type CanvasDrawEvent = {
-  type: 'blur'
-  payload: [startX: number, startY: number, endX: number, endY: number]
-} | {
-  type: 'black-out'
-  payload: [startX: number, startY: number, endX: number, endY: number]
-}
+class EventSource<EventItem extends { type: string; payload: unknown[] }> {
+  events: EventItem[] = [];
+  currentIndex = 0;
 
-class EventSource<EventItem extends { type: string, payload: unknown[] }> {
-  events: EventItem[] = []
-  currentIndex = 0
+  constructor(
+    private actionByType: Record<
+      string,
+      (...args: EventItem["payload"]) => void
+    >
+  ) {}
 
-  constructor(private actionByType: Record<string, (...args: EventItem['payload']) => void>) {
-    if (typeof actionByType.reset !== 'function') throw new Error('actionByType must implement a reset() method');
+  resetState() {
+    throw new Error("resetState not implemented.");
   }
 
   append(event: EventItem) {
-    this.events[this.currentIndex++] = event
-    this.actionByType[event.type].apply(null, event.payload)
+    this.events[this.currentIndex++] = event;
+    this.actionByType[event.type].apply(null, event.payload);
   }
 
   undo() {
-    this.actionByType.reset()
-    // TODO
-    // this.events.slice(0, --this.currentIndex).forEach(event => this.actionByType[event.type].apply(null, event.payload))
+    this.resetState();
+
+    this.events
+      .slice(0, this.currentIndex - 1)
+      .forEach((event) =>
+        this.actionByType[event.type].apply(null, event.payload)
+      );
+
+    this.currentIndex -= 1;
   }
 }
 
-@customElement('a51-canvas')
+@customElement("a51-canvas")
 export class A51Canvas extends LitElement {
-  isDragging = false
-  startX = 0
-  startY = 0
-  endX = 0
-  endY = 0
+  isDragging = false;
+  startX = 0;
+  startY = 0;
+  endX = 0;
+  endY = 0;
 
-  imageCtx: CanvasRenderingContext2D | null = null
-  image: HTMLCanvasElement | null = null
-  overlayCtx: CanvasRenderingContext2D | null = null
-  overlay: HTMLCanvasElement | null = null
+  imageCtx: CanvasRenderingContext2D | null = null;
+  image: HTMLCanvasElement | null = null;
+  overlayCtx: CanvasRenderingContext2D | null = null;
+  overlay: HTMLCanvasElement | null = null;
 
-  eventSource: EventSource<CanvasDrawEvent> | undefined
+  eventSource: EventSource<CanvasDrawEvent> | undefined;
 
   render() {
     return html`
-      <input type=file @change=${this.handleUpload} />
+      <input type="file" @change=${this.handleUpload} />
+      <button @click=${this.undo}>Undo</button>
       <button @click=${this.handleDownload}>Download</button>
-      <div class=wrapper>
-        <canvas class=overlay width=500 height=500></canvas>
-        <canvas class=image width=500 height=500></canvas>
+      <div class="wrapper">
+        <canvas class="overlay" width="500" height="500"></canvas>
+        <canvas class="image" width="500" height="500"></canvas>
       </div>
-    `
+    `;
   }
 
   firstUpdated(): void {
-    this.image = this.renderRoot.querySelector('.image')
-    this.overlay = this.renderRoot.querySelector('.overlay')
+    this.image = this.renderRoot.querySelector(".image");
+    this.overlay = this.renderRoot.querySelector(".overlay");
 
-    if (this.image === null) throw new Error('.image querySelector is null')
-    if (this.overlay === null) throw new Error('.overlay querySelector is null')
+    if (this.image === null) throw new Error(".image querySelector is null");
+    if (this.overlay === null)
+      throw new Error(".overlay querySelector is null");
 
-    this.imageCtx = this.image.getContext('2d', { willReadFrequently: true })
-    this.overlayCtx = this.overlay.getContext('2d')
+    this.imageCtx = this.image.getContext("2d", { willReadFrequently: true });
+    this.overlayCtx = this.overlay.getContext("2d");
 
-    if (this.imageCtx === null) throw new Error('imageCtx is null')
-    if (this.overlayCtx === null) throw new Error('overlayCtx is null')
+    if (this.imageCtx === null) throw new Error("imageCtx is null");
+    if (this.overlayCtx === null) throw new Error("overlayCtx is null");
 
-    this.overlay.addEventListener('mousemove', (e) => {
+    this.overlay.addEventListener("mousemove", (e) => {
       if (!this.isDragging) return;
       this.endX = e.offsetX;
       this.endY = e.offsetY;
       this.drawSelectionRect();
     });
 
-    this.overlay.addEventListener('mousedown', (e) => {
+    this.overlay.addEventListener("mousedown", (e) => {
       if (this.overlay === null) return;
 
       this.isDragging = true;
@@ -108,7 +141,7 @@ export class A51Canvas extends LitElement {
       this.startY = e.clientY - rect.top;
     });
 
-    this.overlay.addEventListener('mouseup', (e) => {
+    this.overlay.addEventListener("mouseup", (e) => {
       if (this.overlay === null) return;
 
       this.isDragging = false;
@@ -117,59 +150,85 @@ export class A51Canvas extends LitElement {
       const _endY = e.clientY - rect.top;
 
       if (_endX < this.startX) {
-        this.endX = this.startX
-        this.startX = _endX
+        this.endX = this.startX;
+        this.startX = _endX;
       }
       if (_endY < this.startY) {
-        this.endY = this.startY
-        this.startY = _endY
+        this.endY = this.startY;
+        this.startY = _endY;
       }
       this.clearSelectionRect();
     });
 
-    const canvasActions = createCanvasActions(this.image, this.imageCtx)
-    this.eventSource = new EventSource<CanvasDrawEvent>(canvasActions)
+    const canvasActions = createCanvasActions(this.image, this.imageCtx);
+    this.eventSource = new EventSource<CanvasDrawEvent>(canvasActions);
+  }
+
+  private undo() {
+    if (this.eventSource === undefined)
+      throw new Error("this.eventSource is undefined");
+    this.eventSource.undo();
   }
 
   private handleUpload(e: Event & { currentTarget: HTMLFormElement }) {
-    if (e.currentTarget.files === null) throw new Error('e.currentTarget.files is null')
+    if (e.currentTarget.files === null)
+      throw new Error("e.currentTarget.files is null");
 
-    // TODO store as this.original image,
-    // di firstUpdated, new EventSource( , reset: () => cara balik ke original image )
-    const src = URL.createObjectURL(e.currentTarget.files[0])
-    const img = new Image()
+    const src = URL.createObjectURL(e.currentTarget.files[0]);
+    const img = new Image();
     img.onload = () => {
-      console.log(this.imageCtx)
-      this.imageCtx?.drawImage(img, 0, 0)
-    }
-    img.src = src
+      this.imageCtx?.drawImage(img, 0, 0);
+    };
+    img.src = src;
+
+    if (this.eventSource === undefined)
+      throw new Error("this.eventSource is undefined");
+
+    this.eventSource.resetState = () => {
+      if (this.imageCtx === null) throw new Error("this.imageCtx is null");
+      
+      this.imageCtx.drawImage(img, 0, 0);
+    };
   }
 
   private handleDownload() {
-    if (this.image === null) throw new Error('this.image is null');
-    document.write(`<img src="${this.image.toDataURL('image/png')}"/>`)
+    if (this.image === null) throw new Error("this.image is null");
+    document.write(`<img src="${this.image.toDataURL("image/png")}"/>`);
   }
 
   private drawSelectionRect() {
-    if (this.overlayCtx === null || this.overlay === null) throw new Error('this.overlayCtx is null');
+    if (this.overlayCtx === null || this.overlay === null)
+      throw new Error("this.overlayCtx is null");
 
     this.overlayCtx.clearRect(0, 0, this.overlay.width, this.overlay.height);
 
-    this.overlayCtx.strokeStyle = 'blue';
+    this.overlayCtx.strokeStyle = "blue";
     this.overlayCtx.lineWidth = 2;
-    this.overlayCtx.strokeRect(this.startX, this.startY, this.endX - this.startX, this.endY - this.startY);
+    this.overlayCtx.strokeRect(
+      this.startX,
+      this.startY,
+      this.endX - this.startX,
+      this.endY - this.startY
+    );
   }
 
   private clearSelectionRect() {
-    if (this.eventSource === undefined) throw new Error('this.eventSource is undefined');
+    if (this.eventSource === undefined)
+      throw new Error("this.eventSource is undefined");
 
     switch (window[controllerCensorOption]) {
-      case 'black-out':
-        return this.eventSource.append({ type: 'black-out', payload: [this.startX, this.startY, this.endX, this.endY] })
-      case 'blur':
-        return this.eventSource.append({ type: 'blur', payload: [this.startX, this.startY, this.endX, this.endY] })
+      case "black-out":
+        return this.eventSource.append({
+          type: "black-out",
+          payload: [this.startX, this.startY, this.endX, this.endY],
+        });
+      case "blur":
+        return this.eventSource.append({
+          type: "blur",
+          payload: [this.startX, this.startY, this.endX, this.endY],
+        });
       default:
-        window[controllerCensorOption] satisfies never
+        window[controllerCensorOption] satisfies never;
     }
   }
 
@@ -179,7 +238,7 @@ export class A51Canvas extends LitElement {
     }
 
     .image {
-      margin: 0 100% 0 0
+      margin: 0 100% 0 0;
     }
 
     .overlay {
@@ -187,11 +246,11 @@ export class A51Canvas extends LitElement {
       left: 0;
       top: 0;
     }
-  `
+  `;
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'a51-canvas': A51Canvas
+    "a51-canvas": A51Canvas;
   }
 }
